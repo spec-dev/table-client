7fc599dcbbee4a835345a7d67ae723780cd01b9f
Ben Whittle <benwhittle31@gmail.com>
2022-08-17 14:58:55 -0500

    v0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import SpecTableClient from './client'
 
 const tableClient = new SpecTableClient()
-const runQuery = tableClient.runQuery
-const streamQuery = tableClient.streamQuery
+const runQuery = (query) => tableClient.runQuery(query)
+const streamQuery = (query) => tableClient.streamQuery(query)
 
 export { SpecTableClient, tableClient, runQuery, streamQuery }
 export { ethereum } from './lib/tables'
